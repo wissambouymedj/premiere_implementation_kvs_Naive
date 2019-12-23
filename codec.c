5756aac6470b2ce5d8f60f7c21221be7111ccd58
TRABZI Mohammed Amine <amine.trabzi@yahoo.com>
2019-12-23 18:16:49 +0100

    codec.c contains structure of KVS code

---
+++ codec.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio>
+#include <string>
+
+int kvs_put(char *key, void * in_data, size_t data_size);
+
+int kvs_get(char *key, void ** out_data, size_t *data_size);
+
+int main(int argc, char **argv)
+{
+	// main code 
+return 0;
+}
+
